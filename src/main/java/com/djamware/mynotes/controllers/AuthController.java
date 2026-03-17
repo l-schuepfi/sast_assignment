1351abd9ca5f43e50ae6601716f1e75260fe09a1
Lilly Schüpferling <lil.schuepferling.21@lehre.mosbach.dhbw.de>
2026-03-17 19:10:52 +0100

    Fix: remove unused imports (java:S1128 src/main/java/com/djamware/mynotes/configs/WebSecurityConfig.java src/main/java/com/djamware/mynotes/controllers/AuthController.java  )

--- src/main/java/com/djamware/mynotes/controllers/AuthController.java
+++ src/main/java/com/djamware/mynotes/controllers/AuthController.java
@@ -1,19 +1,16 @@
 package com.djamware.mynotes.controllers;
 
-import javax.validation.Valid;
-
+import com.djamware.mynotes.models.User;
 import com.djamware.mynotes.models.UserDTO;
+import com.djamware.mynotes.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.djamware.mynotes.models.User;
-import com.djamware.mynotes.services.CustomUserDetailsService;
+import javax.validation.Valid;
 
 @Controller
 public class AuthController {
