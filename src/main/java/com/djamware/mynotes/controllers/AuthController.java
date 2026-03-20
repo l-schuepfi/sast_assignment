b39a25359df8bdfb7a4a5e44867db4e72fb9b211
Lilly Schüpferling <lil.schuepferling.21@lehre.mosbach.dhbw.de>
2026-03-20 15:57:18 +0100

    Fix: javax to jakarta (l.13 src/main/java/com/djamware/mynotes/controllers/AuthController.java)

--- src/main/java/com/djamware/mynotes/controllers/AuthController.java
+++ src/main/java/com/djamware/mynotes/controllers/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 @Controller
 public class AuthController {
