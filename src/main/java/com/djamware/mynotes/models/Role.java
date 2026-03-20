c12c9e07d4d5cdf125f75c810cef16e7487f2ff6
Lilly Schüpferling <lil.schuepferling.21@lehre.mosbach.dhbw.de>
2026-03-20 15:58:04 +0100

    Fix: javax to jakarta (l.5-10 src/main/java/com/djamware/mynotes/models/Role.java)

--- src/main/java/com/djamware/mynotes/models/Role.java
+++ src/main/java/com/djamware/mynotes/models/Role.java
@@ -2,12 +2,12 @@ package com.djamware.mynotes.models;
 
 import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "role")
