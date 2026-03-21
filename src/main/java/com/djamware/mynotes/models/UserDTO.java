aa3c2b613d65e107a4e78c42d5a7b957263a49aa
Lilly Schüpferling <lil.schuepferling.21@lehre.mosbach.dhbw.de>
2026-03-21 22:19:07 +0100

    Fix: add variables, constructor and get/set-methods (src/main/java/com/djamware/mynotes/models/UserDTO.java)

--- src/main/java/com/djamware/mynotes/models/UserDTO.java
+++ src/main/java/com/djamware/mynotes/models/UserDTO.java
@@ -4,8 +4,31 @@ public class UserDTO {
 
 	private String email;
 
+	private String password;
+
+	private String fullname;
+
 	public String getEmail() {
 		return email;
 	}
 
+	public String getPassword() { return password; }
+
+	public String getFullname() { return fullname; }
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public UserDTO() {
+		//needed
+	}
 }
